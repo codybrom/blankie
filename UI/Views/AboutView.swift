97e3e427e89b7eb573f8e88f2a480500bdce458c
Cody Bromley <codybrom@users.noreply.github.com>
2025-01-07 23:08:18 -0600

    license: add more detail to licensing information in about view credits

--- UI/Views/AboutView.swift
+++ UI/Views/AboutView.swift
@@ -33,19 +33,18 @@ struct AboutView: View {
 
       developerSection
 
-      Text("© 2025 Cody Bromley")
+      Text("© 2025 Cody Bromley. All rights reserved.")
         .font(.caption)
 
       // GroupBox with internal scroll
       GroupBox {
         ScrollView {
           VStack(alignment: .leading, spacing: 16) {
-            softwareLicenseSection
+            soundCreditsSection
 
             Divider()
 
-            soundCreditsSection
-            editingNoteSection
+            softwareLicenseSection
           }
           .frame(width: 400)
           .padding(.vertical, 4)
@@ -187,21 +186,20 @@ struct AboutView: View {
       .font(.system(size: 12))
     }
   }
-
-  private var editingNoteSection: some View {
-    Text(
-      "Note: Sound editing involved optimizing audio levels and characteristics according to established guidelines for ambient sound playback."
-    )
-    .font(.system(size: 11))
-    .foregroundStyle(.secondary)
-    .italic()
-  }
 }
 
 private var softwareLicenseSection: some View {
   VStack(alignment: .leading, spacing: 8) {
     Text(
       "This application comes with absolutely no warranty. This program is free software: you can redistribute it and/or modify it under the terms of the MIT License."
+    )
+    .font(.system(size: 12))
+    Text(
+      "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software."
+    )
+    .font(.system(size: 12))
+    Text(
+      "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE."
     )
     .font(.system(size: 12))
     Link(

abba44db8104c1e180a702ceab057ea49eb771f2
Cody Bromley <codybrom@users.noreply.github.com>
2025-01-10 15:00:09 -0600

    feat(docs): update Docker script and update Astro dependencies

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,20 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), playformCompress()],
   site: "https://blankie.rest",
-  server: {
-    headers: {
-      // Set cache-control headers for all static assets
-      "/*.{js,css,jpg,jpeg,png,gif,ico,svg,webp,mp4}":
-        "Cache-Control: public, max-age=31536000, immutable", // 1 year cache
-      // Specific headers for HTML files
-      "/*.html": "Cache-Control: public, max-age=3600", // 1 hour cache
-    },
-  },
+  output: "static",
 });
